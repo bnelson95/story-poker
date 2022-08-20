c0171cc3768968127164d23a094a62a0dae9a32c
Blake Nelson <bnelson95@gmail.com>
2022-08-20 12:13:59 -0400

    Update Api.ts

--- ritualis-frontend/src/services/Api.ts
+++ ritualis-frontend/src/services/Api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default () => {
   axios.defaults.withCredentials = true
   return axios.create({
-    baseURL: 'http://localhost:3000/api', // TODO make this an env var?
+    baseURL: '/api',
     headers: {
       // 'Access-Control-Allow-Origin': '*',
       // 'Access-Control-Allow-Credentials': true,
