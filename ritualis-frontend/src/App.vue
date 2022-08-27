0c6d89f72ffdb870af8a4664f13d3a0e6da970e2
Blake Nelson <bnelson95@gmail.com>
2022-08-27 12:01:46 -0400

    Rebrand to Story Poker

--- ritualis-frontend/src/App.vue
+++ ritualis-frontend/src/App.vue
@@ -29,7 +29,7 @@ import { RouterLink, RouterView } from "vue-router";
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-sm">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">Story Poker</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
